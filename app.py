68d782846d66cf8931518eb6728cf876447687cc
fdlangrh <fadilanugrah751@gmail.com>
2023-10-21 14:08:03 +0700

    First Commit

--- app.py
+++ app.py
@@ -5,13 +5,23 @@ from datetime import datetime
 from bson import ObjectId
 import os 
 from os.path import join, dirname
+from dotenv import load_dotenv
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("mongodb+srv://fadil123:fadil123@atlascluster.jbkflub.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("client.pkl2")
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
-uri = "mongodb+srv://fadil123:fadil123@atlascluster.jbkflub.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(uri)
-db=client.pkl2
-
 
 @app.route('/')
 def home():
